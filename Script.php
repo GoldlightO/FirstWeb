9d7811624d582f3538b954551fd56d221106a6b1
GoldlightO <81767268+GoldlightO@users.noreply.github.com>
2021-09-16 00:55:35 +0300

    Add files via upload

--- Script.php
+++ Script.php
@@ -1,54 +1,28 @@
 <?php
-session_start();
 $start = microtime(true);
 date_default_timezone_set("Europe/Moscow");
 $current_time = date("H:i:s");
-$X = $_GET['X'];
-$Y = $_GET['Y'];
-$R = $_GET['R'];
-
+$x = floatval(htmlspecialchars($_GET["x"]));
+$y = floatval(htmlspecialchars($_GET["y"]));
+$r = floatval(htmlspecialchars($_GET["r"]));
+$class = "No";
 $true = "Точка находится в зоне";
 $false = "Точка находится вне зоны";
-
 $message = "";
 
-if ($X > $R || $X < -$R / 2 || $Y > $R / 2 || $Y < -$R)
+if ($x > $r || $x < -$r / 2 || $y > $r / 2 || $y < -$r)
     $message = $false;
-else if ($X < 0 && $Y > 0)
+else if ($x < 0 && $y > 0)
     $message = $false;
-else if ($X < 0 && $Y > 0 && $X > $R / sqrt(2) && $Y < -$R / sqrt(2))
+else if ($x < 0 && $y > 0 && $x > $r / sqrt(2) && $y < -$r / sqrt(2))
     $message = $false;
-else if ($X < 0 && $Y < 0 && $X * 2 < $Y)
+else if ($x < 0 && $y < 0 && $x * 2 < $y)
     $message = $false;
-else if (($X < 0 && $Y < 0) && ((-2 * $X >= 0 && -$Y >= 0 && 2 + 2 * $X + $Y >= 0) || (-2 * $X <= 0 && -$Y <= 0 && 2 + 2 * $X + $Y <= 0)))
+else if (($x < 0 && $y < 0) && ((-2 * $x >= 0 && -$y >= 0 && 2 + 2 * $x + $y >= 0) || (-2 * $x <= 0 && -$y <= 0 && 2 + 2 * $x + $y <= 0)))
     $message = $true;
-else
+else{
+    $class = "Yes";
     $message = $true;
-
-$time = number_format(microtime(true) - $start, 10, ".", "") * 1000 . 'ms';
-
-// Сохранение в сессию
-$result = array($X, $Y, $R, $message, $time, $current_time);
-if (!isset($_SESSION['results'])) {
-    $_SESSION['results'] = array();
 }
-array_push($_SESSION['results'], $result);
-?>
 
-<?php
-if (isset($_SESSION['results'])) {
-    foreach ($_SESSION['results'] as $result) { ?>
-        <table class="secondTable">
-            <tr>
-                <td><?php echo $result[0] ?></td>
-                <td><?php echo $result[1] ?></td>
-                <td><?php echo $result[2] ?></td>
-                <td><?php echo $result[3] ?></td>
-                <td><?php echo $result[4] ?></td>
-                <td><?php echo $result[5] ?></td>
-            </tr>
-        </table>
-    <?php }
-} ?>
-<?php
-unset($_SESSION['results']); ?>
+print_r('<tr><td>'.$x.'</td><td>'.$y.'</td><td>'.$r.'</td><td class="'.$class.'">'.$message.'</td><td>'.$current_time.'</td></tr>');
